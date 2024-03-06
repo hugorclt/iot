bf24e4afd5227dde4bf0e4a78008a5aedd438b3b
sleleu <leleu.sebastien@hotmail.fr>
2024-03-07 00:30:15 +0100

    add p1

--- p1/script/worker.sh
+++
@@ -1,4 +0,0 @@
-apt update
-apt install curl -y
-apt install net-tools -y
-curl -sfL https://get.k3s.io | K3S_URL=https://192.168.56.110:6443 K3S_TOKEN=12345 INSTALL_K3S_EXEC='--flannel-iface=eth1' sh -
