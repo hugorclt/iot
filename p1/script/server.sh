62837b7bfe204899f6c7178472579fcc316723e1
Oryzon00 <adrian.mael.jung@gmail.com>
2024-03-07 16:48:01 +0100

    apt install net-tools for ifconfig

--- p1/script/server.sh
+++ p1/script/server.sh
@@ -1,5 +1,6 @@
 apt update
 apt install curl -y
+sudo apt install net-tools -y
 
 echo 'export KUBECONFIG=/etc/rancher/k3s/k3s.yaml' >> /home/vagrant/.profile
 curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE=644 k3S_CLUSTER_INIT=1 INSTALL_K3S_EXEC='--flannel-iface=eth1' sh -
