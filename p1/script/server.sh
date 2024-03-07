4b6ac5f57e87e4e603ca3731a63b5783a53d7b80
Oryzon00 <adrian.mael.jung@gmail.com>
2024-03-07 16:45:46 +0100

    move scripts in folder script

--- p1/script/server.sh
+++ p1/script/server.sh
@@ -1,15 +1,12 @@
 apt update
 apt install curl -y
-apt install net-tools -y
 
-# K3s install
-echo "export KUBECONFIG=/etc/rancher/k3s/k3s.yaml" >> /home/vagrant/.profile
-curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE=644 K3S_CLUSTER_INIT=1 INSTALL_K3S_EXEC='--flannel-iface=eth1' sh -
+echo 'export KUBECONFIG=/etc/rancher/k3s/k3s.yaml' >> /home/vagrant/.profile
+curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE=644 k3S_CLUSTER_INIT=1 INSTALL_K3S_EXEC='--flannel-iface=eth1' sh -
 
-# Kubectl install
+# Install kubectl
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 chmod +x ./kubectl
 mv ./kubectl /usr/local/bin/kubectl
 
-echo -n "access_token: "
-cat /var/lib/rancher/k3s/server/node-token
+sudo cat /var/lib/rancher/k3s/server/token > /vagrant/token
--- p1/script/worker.sh
+++ p1/script/worker.sh
@@ -1,27 +1,11 @@
-SERVER_IP="192.168.56.110"
-SERVER_TOKEN_PATH="/var/lib/rancher/k3s/server/node-token"
-LOCAL_TOKEN_PATH="/home/vagrant/"
-
-
 apt update
 apt install curl -y
-apt install net-tools -y
 
-# Copy the access token from the server node
-mkdir -p "/home/vagrant/"
-
-echo "Fetching token."
-until scp vagrant@${SERVER_IP}:${SERVER_TOKEN_PATH} ${LOCAL_TOKEN_PATH} -y -p vagrant
-do
-    echo "Trying again..."
-    sleep 1
+while [ ! -f /vagrant/token ]; do
+    echo "Waiting for token..."
+    sleep 5
 done
 
-echo "Token fetched."
-
-cat ${LOCAL_TOKEN_PATH}
-
-EXPORT ACCESS_TOKEN=$(cat ${LOCAL_TOKEN_PATH})
-
-# Install K3S
-curl -sfL https://get.k3s.io | K3S_URL=https://192.168.56.110:6443 K3S_TOKEN=${ACCESS_TOKEN} INSTALL_K3S_EXEC='--flannel-iface=eth1' sh -
+TOKEN=$(cat /vagrant/token)
+curl -sfL https://get.k3s.io | K3S_URL=https://192.168.56.110:6443 K3S_TOKEN=$TOKEN INSTALL_K3S_EXEC='--flannel-iface=eth1' sh -
+rm /vagrant/token
