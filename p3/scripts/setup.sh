88a438e9aad4446d3f4dc66149b5df14362d66e1
Sleleu <93100775+Sleleu@users.noreply.github.com>
2024-03-11 23:44:34 +0100

    Update setup.sh

--- p3/scripts/setup.sh
+++ p3/scripts/setup.sh
@@ -12,10 +12,8 @@ kubectl create namespace dev
 sleep 5
 
 # setup argocd
-# kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
-kubectl apply -n argocd -f "../argocd/install.yaml"
-#a modifier
-# kubectl patch -n argocd svc argocd-server jsonpath ("insecure")
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+kubectl patch deployment argocd-server -n argocd --type='json' -p='[{"op": "add", "path": "/spec/template/spec/containers/0/args/-", "value": "--insecure"}]'
 
 # wait for argocd to be ready
 kubectl wait --for=condition=Ready pod --all -n argocd --timeout=360s
@@ -26,7 +24,7 @@ kubectl apply  -n argocd -f "../argocd/ingress.yaml"
 # get password
 export ARGOCD_PASSWD=$(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)
 echo -e "${LIGHT_GREEN}Argocd password: $ARGOCD_PASSWD${END}"
-sleep 5
+sleep 3
 
 # login to argocd
 argocd login localhost:8080 --insecure --username admin --password $ARGOCD_PASSWD --plaintext --grpc-web
