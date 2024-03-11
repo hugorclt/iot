a65df83fe588600f177d81f9eae356de665fb816
hugo <hugo@iot>
2024-03-11 18:56:03 +0100

    finished

--- p3/create_cluster.sh
+++ p3/create_cluster.sh
@@ -1,20 +1,12 @@
 # Create cluster
-k3d cluster create sleleu -p "8888:30080@loadbalancer" -p "8080:80@loadbalancer"
-
-
+k3d cluster create sleleu -p "8080:80@loadbalancer" -p "8888:30080@loadbalancer" 
 kubectl create namespace dev
-
-## Install and setup ArgoCD
 kubectl create namespace argocd
 kubectl apply -n argocd -f ./argocd/install.yaml
 kubectl apply -n argocd -f ./argocd/ingress.yaml
+kubectl apply -n argocd -f ./argocd/app.yaml
 kubectl apply -n argocd -f ./argocd/project.yaml
-#kubectl apply -n argocd -f ./argocd/app.yaml
 
-## Install and setup dev project
-#kubectl apply -f ./dev/deployment-dev.yaml -n dev 
 
-# Get password of admin
-#kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath=”{.data.password}” | base64 -d; echo
-
+kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d && echo
 
--- p3/install_dependencies.sh
+++ p3/install_dependencies.sh
@@ -18,3 +18,7 @@ sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin 
 # Install k3d
 curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
 
+# Install argocd
+#curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
+#sudo install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
+#rm argocd-linux-amd64
